374e64366684929062e16c7e50f02f3cb1c28d7e
Progoktato <oktato.ber@gmail.com>
2023-06-01 07:26:27 +0200

    Add project files.

---
+++ Dij.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNobel
+{
+    public class Dij
+    {
+        int evszam;
+        string tipus;
+        string keresztnev;
+        string vezeteknev;
+
+        public Dij(int evszam, string tipus, string keresztnev, string vezeteknev)
+        {
+            this.evszam = evszam;
+            this.tipus = tipus;
+            this.keresztnev = keresztnev;
+            this.vezeteknev = vezeteknev;
+        }
+
+        public int Evszam { get => evszam;  }
+        public string Tipus { get => tipus;  }
+        public string Keresztnev { get => keresztnev; }
+        public string Vezeteknev { get => vezeteknev; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleNobel
+{
+    internal class Program
+    {
+        private static Dij DijGyarto(string csvSor)
+        {
+            var mezok = csvSor.Split(';');
+            return new Dij(int.Parse(mezok[0]), mezok[1], mezok[2], mezok[3]);
+        }
+
+        static void Main(string[] args)
+        {
+            List<Dij> dijak = File.ReadAllLines("nobel.csv").Skip(1).Select(sor => DijGyarto(sor)).ToList();
+
+            //TODO: Előző 4,6,7,8
+            
+
+        }
+
+    }
+}
